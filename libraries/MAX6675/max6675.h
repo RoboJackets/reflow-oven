80f03e68b2c9427cdf1768c1eef4e322edf9e635
Evan Peterson <petersonev@Evans-MacBook-Pro.local>
2015-04-05 06:12:45 -0400

    Added additional windows

---
+++ libraries/MAX6675/max6675.h
@@ -0,0 +1,26 @@
+// this library is public domain. enjoy!
+// www.ladyada.net/learn/sensors/thermocouple
+
+#ifndef _MAX6675_
+#define _MAX6675_
+
+#if ARDUINO >= 100
+ #include "Arduino.h"
+#else
+ #include "WProgram.h"
+#endif
+
+class MAX6675 {
+ public:
+  MAX6675(int8_t SCLK, int8_t CS, int8_t MISO);
+
+  double readCelsius(void);
+  double readFahrenheit(void);
+  // For compatibility with older versions:
+  double readFarenheit(void) { return readFahrenheit(); }
+ private:
+  int8_t sclk, miso, cs;
+  uint8_t spiread(void);
+};
+
+#endif
